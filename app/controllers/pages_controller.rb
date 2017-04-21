a76d20644ac95839c6f97c021cd6bc5f68d05aff
Marisha Sahay <marishasahay@gmail.com>
2017-04-21 02:53:30 -0700

    Add chart for home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,18 @@ class PagesController < ApplicationController
   def organization
     @teams = Team.all
     @members = Member.all
+    @member_team_aggs = MemberTeamAgg.all
   end
   
   def team_show
     @team = Team.find_by(id: params[:id])
     @won_data = execute_statement("select opponent, won, count(id) from matches where team_id = #{@team.id} group by 1,2")
-    @matches = Match.where("team_id = #{@team.id}")
+    @matches = Match.where(["team_id = ?", "#{@team.id}"])
   end
 
   def profile_show
     @profile = Member.find_by(id: params[:id])
-    # @teams = [['American football', MemberTeamAgg.where(team_id: 1).size], ['Basketball', MemberTeamAgg.where(team_id: 2).size],['Baseball', MemberTeamAgg.where(team_id: 3).size],['Soccer', MemberTeamAgg.where(team_id: 4).size],['Ice hockey', MemberTeamAgg.where(team_id: 5).size],MemberTeamAgg.where("member_id = #{@profile.id}")]
-    @teams = MemberTeamAgg.where("member_id = #{@profile.id}")
-    @matches_played = execute_statement("select team_id, sum(no_of_matches_played) from member_team_aggs where member_id = #{@profile.id} group by 1")
+    @teams = MemberTeamAgg.where(["member_id = ?", "#{@profile.id}"])
   end
 
   def profile
